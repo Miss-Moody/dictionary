6063609b571ae2c94b41d2d9d5cd8c937471bfc3
Miss-Moody <elen.celeste@gmail.com>
2022-10-19 20:15:56 +0200

    added dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
-	let [keyword, setKeyword] = useState(null);
+	let [keyword, setKeyword] = useState("");
 
+	function handleResponse(response) {
+		console.log(response.data[0]);
+	}
+
+	// whenever a user submits the search form the function makes an API call to https://dictionaryapi.dev
 	function search(event) {
 		event.preventDefault();
-		alert(`Searching ${keyword}`);
+		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+		axios.get(apiUrl).then(handleResponse);
 	}
 
 	// whenever a symbol is being typed in the searchfield this function uses state to update the keyword
