01e0ed8d8b86baddc9a8863d709b4dec476854fa
Miss-Moody <elen.celeste@gmail.com>
2022-10-25 14:00:15 +0200

    updated phonetics

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 export default function Phonetic(props) {
-	return (
-		<div className="Phonetic">
-			<a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-				Listen
-			</a>
-			<br />
-			{props.phonetic.text}
-		</div>
-	);
+	if (props.phonetic.audio) {
+		return (
+			<div className="Phonetic">
+				<a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+					Listen
+				</a>
+				<br />
+				{props.phonetic.text}
+			</div>
+		);
+	} else {
+		return <div>{props.phonetic.text}</div>;
+	}
 }
