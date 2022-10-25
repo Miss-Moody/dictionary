46fe4587a11155ba44d7dcff70a5dcb18521fdd7
Miss-Moody <elen.celeste@gmail.com>
2022-10-25 14:32:20 +0200

    added audio button

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 export default function Phonetic(props) {
+	var myAudio = new Audio(props.phonetic.audio);
+	function playAudio() {
+		myAudio.play();
+	}
+
 	if (props.phonetic.audio) {
 		return (
 			<div className="Phonetic">
 				<a href={props.phonetic.audio} target="_blank" rel="noreferrer">
 					Listen
 				</a>
+				<button onClick={playAudio} type="button">
+					Play Audio
+				</button>
 				<br />
 				{props.phonetic.text}
 			</div>
